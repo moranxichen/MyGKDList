4aa89424f54baa4fca2631417f95fdc9cc798937
Gavin.Z <moranxichen@live.com>
2026-01-08 11:35:59 +0800

    Refactor ad group and update matching rules

--- src/apps/com.yoloho.dayima.ts
+++ src/apps/com.yoloho.dayima.ts
@@ -6,21 +6,14 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-弹窗广告',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
+      name: "弹窗广告",
       rules: [
         {
-          key: 0,
-          name: '腾讯SDK',
-          fastQuery: true,
-          activityIds: '.activity.MainPageActivity',
-          matches:
-            '@ImageView[childCount=0][text=null][desc=null][id=null][visibleToUser=true][width<90 && height<90] < FrameLayout[childCount=1][text=null][desc=null][id=null][parent.childCount>3] +n FrameLayout >(1,2) [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
-          snapshotUrls: 'https://i.gkd.li/i/13800255',
-        },
-      ],
-    },
+          // 直接使用 vid (即 resource-id 的简写)
+          // 只要 ID 包含 close_button 且可点击即可
+          matches: "[vid='noah_interstitial_close_button'][clickable=true]",
+        }
+      ]
+    }
   ],
 });
